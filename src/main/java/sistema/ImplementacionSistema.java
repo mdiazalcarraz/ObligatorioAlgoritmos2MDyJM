ec1bddd1f8eff9cbdaf31b64300e02f4be7f693d
Mathias Diaz <mathiasdiaz13@gmail.com>
2024-10-10 20:02:25 -0300

    first commit

---
+++ src/main/java/sistema/ImplementacionSistema.java
@@ -0,0 +1,77 @@
+package sistema;
+
+import interfaz.*;
+
+public class ImplementacionSistema implements Sistema {
+
+
+    @Override
+    public Retorno inicializarSistema(int maxSucursales) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno registrarJugador(String alias, String nombre, String apellido, Categoria categoria) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno buscarJugador(String alias) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarJugadoresAscendente() {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarJugadoresPorCategoria(Categoria unaCategoria) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno registrarEquipo(String nombre, String manager) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno agregarJugadorAEquipo(String nombreEquipo, String aliasJugador) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarJugadoresDeEquipo(String nombreEquipo) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarEquiposDescendente() {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno registrarSucursal(String codigo, String nombre) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno registrarConexion(String codigoSucursal1, String codigoSucursal2, int latencia) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno actualizarConexion(String codigoSucursal1, String codigoSucursal2, int latencia) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno analizarSucursal(String codigoSucursal) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno sucursalesParaTorneo(String codigoSucursalAnfitriona, int latenciaLimite) {
+        return Retorno.noImplementada();
+    }
+}
